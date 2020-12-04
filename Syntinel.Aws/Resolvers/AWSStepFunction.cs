f0e5deae81029165b6389d507d2d6a63e178fdfe
Guy Waguespack <guy.waguespack@gmail.com>
2020-12-04 12:23:53 -0600

    Initial AWSStepFunction Resolver upload.

---
+++ Syntinel.Aws/Resolvers/AWSStepFunction.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using Amazon;
+using Amazon.StepFunctions;
+using Amazon.StepFunctions.Model;
+
+using Syntinel.Core;
+using Syntinel.Aws;
+
+public class AWSStepFunctionResolver : IResolver
+{
+    class Config
+    {
+        [JsonProperty(PropertyName = "arn")]
+        public string Arn { get; set; }
+
+        [JsonProperty(PropertyName = "region")]
+        public string Region { get; set; }
+    }
+
+    class ExecutionArn
+    {
+        private string _arn;
+        private string[] _parts;
+
+        public string Arn { get { return _arn; } set { _arn = value; _parts = _arn.Split(':'); } }
+        public string Region { get { return _parts[3]; } }
+        public string Account { get { return _parts[4]; } }
+        public string FunctionName { get { return _parts[6]; } }
+        public string Name { get { return _parts[7]; } }
+
+        public ExecutionArn(string arn)
+        {
+            this.Arn = arn;
+        }
+    }
+
+    public Status ProcessRequest(ResolverRequest request)
+    {
+        String cueId = request.CueId;
+        Config config = JsonTools.Convert<Config>(request.Signal.Cues[cueId].Resolver.Config);
+        RegionEndpoint region = RegionEndpoint.GetBySystemName(System.Environment.GetEnvironmentVariable("AWS_REGION"));
+        if (!String.IsNullOrEmpty(config.Region))
+            region = RegionEndpoint.GetBySystemName(config.Region);
+
+        AmazonStepFunctionsClient client = new AmazonStepFunctionsClient(region);   // Set Region
+
+        StartExecutionRequest exeRequest = new StartExecutionRequest
+        {
+            StateMachineArn = config.Arn,
+            Input = JsonTools.Serialize(request)
+        };
+
+        Task<StartExecutionResponse> t = client.StartExecutionAsync(exeRequest);
+        t.Wait(30000);
+        StartExecutionResponse response = t.Result;
+
+        Status status = new Status();
+        status.Id = request.Id;
+        status.ActionId = request.ActionId;
+        if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+        {
+            ExecutionArn eArn = new ExecutionArn(response.ExecutionArn);
+            status.NewStatus = StatusType.SentToResolver;
+            status.Message = $"Request Sent To Step Function [{eArn.FunctionName}].  Execution Name [{eArn.Name}].";
+        }
+        else
+        {
+            status.NewStatus = StatusType.Error;
+            status.Message = $"Error Sending To Step Function [{config.Arn}].  {response.HttpStatusCode}";
+        }
+
+        return status;
+    }
+}
+
--- Syntinel.Tester/Program.cs
+++ Syntinel.Tester/Program.cs
@@ -24,36 +24,41 @@ namespace Syntinel.Tester
             Processor processor = new Processor(db);
             ILogger logger = new ConsoleLogger();
 
-            int testCount = 10;
 
             /*** Send Signal Message ***/
-            TextReader reader = new StreamReader(new FileStream(@"/Users/guy/Documents/Source/syntinel.core.net/Syntinel.Tester/TestFiles/Signal-UsingTemplate.json", FileMode.Open));
-            string fileStr = reader.ReadToEnd();
-            Signal signal = JsonConvert.DeserializeObject<Signal>(fileStr);
-            SignalReply reply = processor.ProcessSignal(signal);
-            Console.WriteLine($"Status : {reply.StatusCode}");
-            foreach (SignalStatus status in reply.Results)
-                Console.WriteLine($"     {status.ChannelId} : {status.Code} - {status.Message}");
+            //int signalCount = 1;
+            //TextReader reader = new StreamReader(new FileStream(@"/Users/guy/Documents/Source/syntinel.core.net/Syntinel.Tester/TestFiles/Signal-UsingTemplate.json", FileMode.Open));
+            //string fileStr = reader.ReadToEnd();
+            //Signal signal = JsonConvert.DeserializeObject<Signal>(fileStr);
+            //Parallel.For(0, signalCount, index =>
+            //{
+            //    SignalReply reply = processor.ProcessSignal(signal);
+            //    Console.WriteLine($"Status : {reply.StatusCode}");
+            //    foreach (SignalStatus status in reply.Results)
+            //        Console.WriteLine($"     {status.ChannelId} : {status.Code} - {status.Message}");
+            //});
 
 
             /*** Send Teams Cue Response ***/
-            //TextReader reader = new StreamReader(new FileStream(@"/Users/guy/Documents/Source/syntinel.core.net/Syntinel.Tester/TestFiles/Cue-Teams.json", FileMode.Open));
-            //string fileStr = reader.ReadToEnd();
-            //Dictionary<string, object> reply = JsonConvert.DeserializeObject<Dictionary<string, object>>(fileStr);
-            //Cue cue = Teams.CreateCue(reply);
-            //Parallel.For(0, testCount, index =>
-            // {
-            //     string num = $"{index}".PadLeft(4, '0');
-            //     CueReply cueReply = processor.ReceiveCue(cue);
-            //     Console.WriteLine($"{num} - {JsonTools.Serialize(cueReply)}");
-            // });
+            int cueCount = 1;
+            TextReader reader = new StreamReader(new FileStream(@"/Users/guy/Documents/Source/syntinel.core.net/Syntinel.Tester/TestFiles/Cue-Teams.json", FileMode.Open));
+            string fileStr = reader.ReadToEnd();
+            Dictionary<string, object> reply = JsonConvert.DeserializeObject<Dictionary<string, object>>(fileStr);
+            Cue cue = Teams.CreateCue(reply);
+            Parallel.For(0, cueCount, index =>
+             {
+                 string num = $"{index}".PadLeft(4, '0');
+                 CueReply cueReply = processor.ReceiveCue(cue);
+                 Console.WriteLine($"{num} - {JsonTools.Serialize(cueReply)}");
+             });
 
 
             /*** Stress Test Status Messages ***/
+            //int statusCount = 1;
             //string signalId = "000000000";
             //string actionId = "0ZC6BJ28Z";
 
-            //Parallel.For(1, testCount, index =>
+            //Parallel.For(1, statusCount, index =>
             //{
             //    Status status = new Status();
             //    status.Id = signalId;
