e407708957d4ad368b350823bd40fa12d7325d3f
Guy Waguespack <guy.waguespack@gmail.com>
2020-12-10 16:29:40 -0600

    Updated Copyright Years

--- Syntinel.Core/Properties/AssemblyInfo.cs
+++ Syntinel.Core/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Syntinel.Core")]
-[assembly: AssemblyCopyright("Copyright ? Guy Waguespack, 2020 - 2022")]
+[assembly: AssemblyCopyright("Copyright ? Guy Waguespack, 2019 - 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,8 +17,8 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.20345.0")]
+[assembly: AssemblyFileVersion("0.1.20345.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
